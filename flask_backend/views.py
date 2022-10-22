8122b99d36cd37f65bfd7962ab4dfedfca6f908e
Hooman Sherkat <3044447+hsherkat@users.noreply.github.com>
2022-10-22 13:28:08 -0700

    moved components into folder, made Header component, styled it

--- flask_backend/views.py
+++ flask_backend/views.py
@@ -1,4 +1,8 @@
-import time
+from datetime import datetime
+
+import pytz
+from flask import request
+from timezonefinder import TimezoneFinder
 
 from flask_backend import app
 
@@ -10,4 +14,28 @@ def home_page():
 
 @app.route("/api")
 def api_fn():
-    return {"time": time.time()}
+    tf = TimezoneFinder()
+    lat, lng = request.args.get("latitude"), request.args.get("longitude")
+    if lat and lng:
+        tz = tf.timezone_at(lng=float(lng), lat=float(lat))
+        current_time = datetime.now(tz=pytz.timezone(tz))
+        print(current_time)
+        return {"time": current_time.strftime("%A, %B %d %I:%M:%S")}
+    return {"time": datetime.now()}
+
+
+@app.route("/api2", methods=["GET", "POST"])
+def api2_fn():
+    tf = TimezoneFinder()
+    lat = lng = None
+    if request.method == "GET":
+        lat, lng = request.args.get("latitude"), request.args.get("longitude")
+    elif request.method == "POST":
+        json = request.get_json(silent=True)
+        lat, lng = json["latitude"], json["longitude"]
+    if lat and lng:
+        tz = tf.timezone_at(lng=float(lng), lat=float(lat))
+        current_time = datetime.now(tz=pytz.timezone(tz))
+        print(current_time)
+        return {"time": current_time.strftime("%A, %B %d %I:%M:%S")}
+    return {"time": datetime.now()}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-navigator.geolocation.getCurrentPosition((position) => {
-  console.log(position);
-});
 
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <App />
