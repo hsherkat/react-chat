6ca998bf354ae6adc51b3767fb977ed9f7a6f812
Hooman Sherkat <3044447+hsherkat@users.noreply.github.com>
2022-11-10 10:16:52 -0800

    changed CORS to *

--- flask_backend/__init__.py
+++ flask_backend/__init__.py
@@ -2,9 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__, static_folder="../build", static_url_path="/")
-socket = SocketIO(
-    app, cors_allowed_origins=["http://localhost:3000", "http://localhost:5000"]
-)
+socket = SocketIO(app, cors_allowed_origins="*")
 
 from flask_backend.views import *
 from flask_backend.events import *
